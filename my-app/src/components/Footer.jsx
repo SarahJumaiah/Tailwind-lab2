96ed9d63dd96e0747db3637317504851fb589672
sarahjumaiah <sarahjumaiah@gmail.com>
2024-09-26 15:02:01 +0300

    done

---
+++ my-app/src/components/Footer.jsx
@@ -0,0 +1,15 @@
+function Footer() {
+  return (
+    <footer style={{ backgroundColor: "#303030" }} className="text-white py-4">
+      <div className="container mx-auto flex justify-between items-center px-4">
+        <h1 className="text-lg font-bold">FreshGoods</h1>
+
+        <p className="text-sm">
+          &copy; 2020 Insert Name Here. All rights reserved.
+        </p>
+      </div>
+    </footer>
+  );
+}
+
+export default Footer;
